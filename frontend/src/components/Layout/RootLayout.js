5894313ed9635f3b73cfaa45ff980caebac034a9
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-08-22 15:15:53 +0300

    feat: reflecting the current state navigation UI
    using useNavigation() hook by react-router-dom, we can handle the current state navigation (i.e. our request on the server) and showing loading state.
    
    With this hook, we can handle events by accessing the state, like:
    - idle
    - loading
    - submitting

--- frontend/src/components/Layout/RootLayout.js
+++ frontend/src/components/Layout/RootLayout.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 import MainNavigation from '../MainNavigation';
 
 const RootLayout = () => {
+  const navigation = useNavigation();
+
   return (
     <React.Fragment>
       <MainNavigation />
       <main>
-        <Outlet />
+        {navigation.state === 'loading' ? (
+          <div style={{ textAlign: 'center' }}>
+            <p>Loading ...</p>
+          </div>
+        ) : (
+          <Outlet />
+        )}
       </main>
     </React.Fragment>
   );
